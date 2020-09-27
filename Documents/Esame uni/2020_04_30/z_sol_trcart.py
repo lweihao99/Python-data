40ada51851036cf2c2c3f23ad425c3fdf34a3607
Dr-Zen <weihaoliu99@gmail.com>
2020-09-27 21:40:36 +0200

    esercitazione 2020-09-27

--- Documents/Esame uni/2020_04_30/z_sol_trcart.py
+++ Documents/Esame uni/2020_04_30/z_sol_trcart.py
@@ -9,8 +9,10 @@ nomeEsercizio = 'ecommerceCart'
 # La (vera) struttura dati caricata dal file sara' più lunga,
 # i primi elementi di entrambe possono essere diversi.
 
-datiCarrelli = [(17, 'ADD', 18, 3), (17, 'ADD', 5, 1), (24, 'ADD', 43, 4), (16, 'ADD', 3, 8), (17, 'ADD', 5, 7), (17, 'ADD', 6, 1), (17, 'DROP', 6, 1), (17, 'ADD', 6, 4), (24, 'ADD', 43, 3), (16, 'DROP', 3, 1), (24, 'CHECKOUT'), (17, 'CHECKOUT'), (29, 'ADD', 26, 9), (16, 'ADD', 3, 9), (14, 'ADD', 40, 8), (14, 'DROP', 40, 5), (14, 'ADD', 51, 3), (14, 'ADD', 59, 2), (14, 'DROP', 59, 2), (14, 'ADD', 59, 5), (14, 'CHECKOUT'), (29, 'ADD', 26, 6), (16, 'CHECKOUT'), (13, 'ADD', 50, 9), (1, 'ADD', 57, 4), (1, 'DROP', 57, 2), (2, 'ADD', 0, 4), (1, 'ADD', 57, 6), (1, 'CHECKOUT'), (5, 'ADD', 8, 1), (8, 'ADD', 45, 6), (21, 'ADD', 17, 6), (13, 'DROP', 50, 2), (5, 'ADD', 8, 5), (5, 'ADD', 57, 1), (5, 'DROP', 57, 1), (5, 'ADD', 57, 9), (5, 'ADD', 49, 5), (22, 'ADD', 4, 6), (5, 'DROP', 49, 3), (28, 'ADD', 17, 2), (5, 'CHECKOUT'), (2, 'DROP', 0, 3), (27, 'ADD', 3, 1), (21, 'DROP', 17, 1), (30, 'ADD', 47, 4), (2, 'ADD', 52, 2), (30, 'DROP', 47, 2), (2, 'DROP', 52, 1), (2, 'ADD', 32, 6), (21, 'CHECKOUT'), (0, 'ADD', 3, 2), (13, 'ADD', 50, 6), (30, 'ADD', 47, 6), (27, 'ADD', 3, 9), (1, 'ADD', 29, 9), (8, 'ADD', 45, 4), (30, 'ADD', 17, 2), (2, 'ADD', 32, 3), (20, 'ADD', 30, 7), (30, 'DROP', 17, 1), (30, 'ADD', 15, 6), (25, 'ADD', 10, 2), (25, 'CHECKOUT'), (22, 'ADD', 12, 1), (28, 'ADD', 5, 9), (30, 'CHECKOUT'), (17, 'ADD', 41, 9), (22, 'ADD', 12, 8), (8, 'ADD', 24, 8), (27, 'CHECKOUT'), (11, 'ADD', 12, 4), (17, 'ADD', 22, 3), (11, 'DROP', 12, 2), (13, 'CHECKOUT'), (8, 'DROP', 24, 3), (0, 'ADD', 3, 4), (0, 'ADD', 56, 4), (2, 'CHECKOUT'), (1, 'ADD', 29, 5), (1, 'ADD', 13, 8), (11, 'ADD', 12, 3), (17, 'DROP', 22, 2), (11, 'CHECKOUT'), (3, 'ADD', 17, 4), (25, 'ADD', 34, 9), (28, 'DROP', 5, 8), (19, 'ADD', 6, 7), (28, 'CHECKOUT'), (3, 'DROP', 17, 1), (1, 'DROP', 13, 1), (29, 'ADD', 23, 9), (0, 'ADD', 32, 3), (8, 'ADD', 24, 3), (0, 'DROP', 32, 2), (0, 'CHECKOUT'), (25, 'CHECKOUT'), (22, 'CHECKOUT'), (3, 'ADD', 25, 8), (3, 'ADD', 15, 4), (8, 'ADD', 43, 7), (15, 'ADD', 7, 9), (18, 'ADD', 53, 6), (6, 'ADD', 58, 7), (18, 'ADD', 26, 8), (17, 'ADD', 22, 8), (29, 'CHECKOUT'), (20, 'DROP', 30, 1), (1, 'CHECKOUT'), (8, 'CHECKOUT'), (18, 'CHECKOUT'), (17, 'ADD', 9, 5), (28, 'ADD', 15, 9), (3, 'DROP', 15, 4), (30, 'ADD', 50, 5), (5, 'ADD', 31, 7), (20, 'ADD', 30, 7), (28, 'DROP', 15, 8), (3, 'ADD', 15, 4), (5, 'DROP', 31, 2), (28, 'ADD', 15, 8), (28, 'CHECKOUT'), (4, 'ADD', 29, 5), (10, 'ADD', 31, 8), (3, 'CHECKOUT'), (30, 'ADD', 50, 7), (20, 'ADD', 40, 5), (10, 'DROP', 31, 3), (19, 'ADD', 6, 1), (30, 'ADD', 27, 8), (5, 'CHECKOUT'), (14, 'ADD', 9, 5), (9, 'ADD', 29, 6), (22, 'ADD', 2, 5), (17, 'CHECKOUT'), (6, 'ADD', 58, 4), (15, 'DROP', 7, 1), (4, 'DROP', 29, 5), (19, 'ADD', 51, 3), (19, 'DROP', 51, 2), (20, 'DROP', 40, 4), (25, 'ADD', 17, 1), (12, 'ADD', 11, 1), (12, 'CHECKOUT'), (30, 'ADD', 27, 7), (25, 'CHECKOUT'), (30, 'CHECKOUT')] 
-composizioneCarrelli = [(24, {43: 7}), (17, {18: 3, 5: 8, 6: 4}), (14, {40: 3, 51: 3, 59: 5}), (16, {3: 16}), (1, {57: 8}), (5, {8: 6, 57: 9, 49: 2}), (21, {17: 5}), (25, {10: 2}), (30, {17: 1, 47: 8, 15: 6}), (27, {3: 10}), (13, {50: 13}), (2, {0: 1, 32: 9, 52: 1}), (11, {12: 5}), (28, {17: 2, 5: 1}), (0, {56: 4, 32: 1, 3: 6}), (25, {34: 9}), (22, {4: 6, 12: 9}), (29, {26: 15, 23: 9}), (1, {29: 14, 13: 7}), (8, {24: 8, 43: 7, 45: 10}), (18, {26: 8, 53: 6}), (28, {15: 9}), (3, {17: 3, 25: 8, 15: 4}), (5, {31: 5}), (17, {41: 9, 22: 9, 9: 5}), (12, {11: 1}), (25, {17: 1}), (30, {50: 12, 27: 15}), (20, {40: 1, 30: 13}), (5, {2: 7, 29: 3}), (12, {26: 8}), (10, {0: 8, 31: 5}), (14, {9: 14}), (9, {29: 6}), (8, {2: 6, 3: 4}), (0, {0: 5, 8: 12, 21: 7}), (15, {7: 8, 15: 8}), (8, {45: 5}), (14, {7: 4}), (28, {0: 5, 20: 7, 15: 2}), (1, {0: 7, 57: 3, 43: 7}), (22, {2: 3}), (29, {1: 7, 21: 3}), (10, {20: 2}), (0, {12: 3}), (25, {25: 5, 3: 12}), (7, {3: 1, 45: 2, 29: 8}), (17, {40: 5, 3: 8}), (30, {13: 17, 54: 3}), (11, {19: 17, 58: 1, 27: 5}), (27, {33: 1, 34: 13, 28: 9}), (19, {51: 1, 37: 7, 6: 8}), (6, {57: 15, 58: 11}), (18, {18: 3, 2: 15, 13: 9}), (21, {35: 2}), (10, {0: 1, 48: 7, 42: 8}), (2, {49: 11, 34: 1, 4: 1}), (28, {31: 4, 30: 5, 39: 2}), (14, {8: 8, 32: 4, 58: 2}), (0, {1: 3, 10: 4, 47: 11}), (15, {42: 4, 58: 12, 54: 3}), (29, {58: 4, 45: 13, 38: 11}), (8, {56: 3, 57: 2, 47: 12}), (1, {0: 13, 4: 6, 44: 8}), (23, {38: 2}), (21, {52: 10, 53: 3}), (28, {16: 7}), (7, {5: 1})]  # ****** da aggiungere
+datiCarrelli = [(17, 'ADD', 18, 3), (17, 'ADD', 5, 1), (24, 'ADD', 43, 4), (16, 'ADD', 3, 8), (17, 'ADD', 5, 7), (17, 'ADD', 6, 1), (17, 'DROP', 6, 1), (17, 'ADD', 6, 4), (24, 'ADD', 43, 3), (16, 'DROP', 3, 1), (24, 'CHECKOUT'), (17, 'CHECKOUT'), (29, 'ADD', 26, 9), (16, 'ADD', 3, 9), (14, 'ADD', 40, 8), (14, 'DROP', 40, 5), (14, 'ADD', 51, 3), (14, 'ADD', 59, 2), (14, 'DROP', 59, 2), (14, 'ADD', 59, 5), (14, 'CHECKOUT'), (29, 'ADD', 26, 6), (16, 'CHECKOUT'), (13, 'ADD', 50, 9), (1, 'ADD', 57, 4), (1, 'DROP', 57, 2), (2, 'ADD', 0, 4), (1, 'ADD', 57, 6), (1, 'CHECKOUT'), (5, 'ADD', 8, 1), (8, 'ADD', 45, 6), (21, 'ADD', 17, 6), (13, 'DROP', 50, 2), (5, 'ADD', 8, 5), (5, 'ADD', 57, 1), (5, 'DROP', 57, 1), (5, 'ADD', 57, 9), (5, 'ADD', 49, 5), (22, 'ADD', 4, 6), (5, 'DROP', 49, 3), (28, 'ADD', 17, 2), (5, 'CHECKOUT'), (2, 'DROP', 0, 3), (27, 'ADD', 3, 1), (21, 'DROP', 17, 1), (30, 'ADD', 47, 4), (2, 'ADD', 52, 2), (30, 'DROP', 47, 2), (2, 'DROP', 52, 1), (2, 'ADD', 32, 6), (21, 'CHECKOUT'), (0, 'ADD', 3, 2), (13, 'ADD', 50, 6), (30, 'ADD', 47, 6), (27, 'ADD', 3, 9), (1, 'ADD', 29, 9), (8, 'ADD', 45, 4), (30, 'ADD', 17, 2), (2, 'ADD', 32, 3), (20, 'ADD', 30, 7), (30, 'DROP', 17, 1), (30, 'ADD', 15, 6), (25, 'ADD', 10, 2), (25, 'CHECKOUT'), (22, 'ADD', 12, 1), (28, 'ADD', 5, 9), (30, 'CHECKOUT'), (17, 'ADD', 41, 9), (22, 'ADD', 12, 8), (8, 'ADD', 24, 8), (27, 'CHECKOUT'), (11, 'ADD', 12, 4), (17, 'ADD', 22, 3),
+                (11, 'DROP', 12, 2), (13, 'CHECKOUT'), (8, 'DROP', 24, 3), (0, 'ADD', 3, 4), (0, 'ADD', 56, 4), (2, 'CHECKOUT'), (1, 'ADD', 29, 5), (1, 'ADD', 13, 8), (11, 'ADD', 12, 3), (17, 'DROP', 22, 2), (11, 'CHECKOUT'), (3, 'ADD', 17, 4), (25, 'ADD', 34, 9), (28, 'DROP', 5, 8), (19, 'ADD', 6, 7), (28, 'CHECKOUT'), (3, 'DROP', 17, 1), (1, 'DROP', 13, 1), (29, 'ADD', 23, 9), (0, 'ADD', 32, 3), (8, 'ADD', 24, 3), (0, 'DROP', 32, 2), (0, 'CHECKOUT'), (25, 'CHECKOUT'), (22, 'CHECKOUT'), (3, 'ADD', 25, 8), (3, 'ADD', 15, 4), (8, 'ADD', 43, 7), (15, 'ADD', 7, 9), (18, 'ADD', 53, 6), (6, 'ADD', 58, 7), (18, 'ADD', 26, 8), (17, 'ADD', 22, 8), (29, 'CHECKOUT'), (20, 'DROP', 30, 1), (1, 'CHECKOUT'), (8, 'CHECKOUT'), (18, 'CHECKOUT'), (17, 'ADD', 9, 5), (28, 'ADD', 15, 9), (3, 'DROP', 15, 4), (30, 'ADD', 50, 5), (5, 'ADD', 31, 7), (20, 'ADD', 30, 7), (28, 'DROP', 15, 8), (3, 'ADD', 15, 4), (5, 'DROP', 31, 2), (28, 'ADD', 15, 8), (28, 'CHECKOUT'), (4, 'ADD', 29, 5), (10, 'ADD', 31, 8), (3, 'CHECKOUT'), (30, 'ADD', 50, 7), (20, 'ADD', 40, 5), (10, 'DROP', 31, 3), (19, 'ADD', 6, 1), (30, 'ADD', 27, 8), (5, 'CHECKOUT'), (14, 'ADD', 9, 5), (9, 'ADD', 29, 6), (22, 'ADD', 2, 5), (17, 'CHECKOUT'), (6, 'ADD', 58, 4), (15, 'DROP', 7, 1), (4, 'DROP', 29, 5), (19, 'ADD', 51, 3), (19, 'DROP', 51, 2), (20, 'DROP', 40, 4), (25, 'ADD', 17, 1), (12, 'ADD', 11, 1), (12, 'CHECKOUT'), (30, 'ADD', 27, 7), (25, 'CHECKOUT'), (30, 'CHECKOUT')]
+composizioneCarrelli = [(24, {43: 7}), (17, {18: 3, 5: 8, 6: 4}), (14, {40: 3, 51: 3, 59: 5}), (16, {3: 16}), (1, {57: 8}), (5, {8: 6, 57: 9, 49: 2}), (21, {17: 5}), (25, {10: 2}), (30, {17: 1, 47: 8, 15: 6}), (27, {3: 10}), (13, {50: 13}), (2, {0: 1, 32: 9, 52: 1}), (11, {12: 5}), (28, {17: 2, 5: 1}), (0, {56: 4, 32: 1, 3: 6}), (25, {34: 9}), (22, {4: 6, 12: 9}), (29, {26: 15, 23: 9}), (1, {29: 14, 13: 7}), (8, {24: 8, 43: 7, 45: 10}), (18, {26: 8, 53: 6}), (28, {15: 9}), (3, {17: 3, 25: 8, 15: 4}), (5, {31: 5}), (17, {41: 9, 22: 9, 9: 5}), (12, {11: 1}), (25, {17: 1}), (30, {50: 12, 27: 15}), (20, {40: 1, 30: 13}), (5, {2: 7, 29: 3}), (12, {26: 8}), (10, {0: 8, 31: 5}), (14, {9: 14}), (9, {29: 6}), (8, {2: 6, 3: 4}), (0, {0: 5, 8: 12, 21: 7}), (15, {
+    7: 8, 15: 8}), (8, {45: 5}), (14, {7: 4}), (28, {0: 5, 20: 7, 15: 2}), (1, {0: 7, 57: 3, 43: 7}), (22, {2: 3}), (29, {1: 7, 21: 3}), (10, {20: 2}), (0, {12: 3}), (25, {25: 5, 3: 12}), (7, {3: 1, 45: 2, 29: 8}), (17, {40: 5, 3: 8}), (30, {13: 17, 54: 3}), (11, {19: 17, 58: 1, 27: 5}), (27, {33: 1, 34: 13, 28: 9}), (19, {51: 1, 37: 7, 6: 8}), (6, {57: 15, 58: 11}), (18, {18: 3, 2: 15, 13: 9}), (21, {35: 2}), (10, {0: 1, 48: 7, 42: 8}), (2, {49: 11, 34: 1, 4: 1}), (28, {31: 4, 30: 5, 39: 2}), (14, {8: 8, 32: 4, 58: 2}), (0, {1: 3, 10: 4, 47: 11}), (15, {42: 4, 58: 12, 54: 3}), (29, {58: 4, 45: 13, 38: 11}), (8, {56: 3, 57: 2, 47: 12}), (1, {0: 13, 4: 6, 44: 8}), (23, {38: 2}), (21, {52: 10, 53: 3}), (28, {16: 7}), (7, {5: 1})]  # ****** da aggiungere
 
 ##########################################################
 # DESCRIZIONE DEI FILE CON I DATI
@@ -56,7 +58,7 @@ composizioneCarrelli = [(24, {43: 7}), (17, {18: 3, 5: 8, 6: 4}), (14, {40: 3, 5
 #
 #   La prima riga contiene l'intestazione del file.
 #   In ogni record (record e' un sinonimo di 'riga')
-#   può essere memorizzata un'operazione (ADD, DROP o CHECKOUT), 
+#   può essere memorizzata un'operazione (ADD, DROP o CHECKOUT),
 #   maggiori dettagli qua sotto.
 #   Una precisazione: anche se le operazioni svolte dai diversi utenti sono
 #   memorizzate nello stesso file, ogni utente interagisce solo il contenuto del suo
@@ -161,9 +163,9 @@ composizioneCarrelli = [(24, {43: 7}), (17, {18: 3, 5: 8, 6: 4}), (14, {40: 3, 5
 ##########################################################
 #
 # Se vi sorgessero dei dubbi sull'interpretazione delle richieste qua sotto,
-# descrivete in un commento le assunzioni (aggiuntive) sulle quali vi baserete. 
+# descrivete in un commento le assunzioni (aggiuntive) sulle quali vi baserete.
 # Assunzioni in evidente violazione delle richieste qua sotto non saranno
-# considerate valide. 
+# considerate valide.
 
 cognome = 'Sostituiscimi con il cognome'  # inserisci qua il tuo cognome
 nome = 'Sostituiscimi con il nome'  # inserisci qua il tuo nome
@@ -196,29 +198,28 @@ nome = 'Sostituiscimi con il nome'  # inserisci qua il tuo nome
 #   codice).
 #
 def caricaDatiCarrelli(fn):
-    #return datiCarrelli # se non riuscite ad implementare la funzione, potete usare temporaneamente questa struttura dati. Tuttavia se lo fate la funzione sarà considerata non svolta.
+    # return datiCarrelli # se non riuscite ad implementare la funzione, potete usare temporaneamente questa struttura dati. Tuttavia se lo fate la funzione sarà considerata non svolta.
     # Implementa il codice della funzione qua sotto. Questa riga può essere cancellata.
     pass
     fd = open(fn, 'r')
-    ds=[] # struttura dati da restituire
+    ds = []  #  struttura dati da restituire
     #    [ (id_cliente1, 'ADD', id_prodottoA, qtA),
     #          (id_cliente2, 'DROP', id_prodottoB, qtB),
     #          (id_cliente3, 'CHECKOUT'),
-    fd.readline() # salto l'intestazione
+    fd.readline()  #  salto l'intestazione
     for line in fd:
         line = line.strip('\n').strip('\r')
         fields = line.split(';')
         # TIME;CUSTOMER;OPERATION;...;...
         #   00:16:01;17;ADD;18;3
         #   00:37:27;24;CHECKOUT      # Qua c'e' un checkout
-        idCli=int(fields[1])
-        tipoOp=fields[2]
-        if tipoOp=='ADD' or tipoOp=='DROP':
-            ds.append( (idCli, tipoOp, int(fields[3]), int(fields[4]) ) )
-        elif tipoOp=='CHECKOUT':
-            ds.append( (idCli, tipoOp) )
+        idCli = int(fields[1])
+        tipoOp = fields[2]
+        if tipoOp == 'ADD' or tipoOp == 'DROP':
+            ds.append((idCli, tipoOp, int(fields[3]), int(fields[4])))
+        elif tipoOp == 'CHECKOUT':
+            ds.append((idCli, tipoOp))
     return ds
-
 
 
 # - La funzione seguente accetta come parametri in ingresso la struttura dati
@@ -242,37 +243,37 @@ def caricaDatiCarrelli(fn):
 #   checkout.
 #
 def calcolaComposizione(ds):
-    #return composizioneCarrelli # se non riuscite ad implementare la funzione, potete usare temporaneamente questa struttura dati. Tuttavia se lo fate la funzione sarà considerata non svolta.
+    # return composizioneCarrelli # se non riuscite ad implementare la funzione, potete usare temporaneamente questa struttura dati. Tuttavia se lo fate la funzione sarà considerata non svolta.
     # Implementa il codice della funzione qua sotto. Questa riga può essere cancellata.
     pass
-    carrelli={} # dizionario usato per gestire l'inserimento di valori nel carrello
-    tr=[] # struttura dati da restituire
+    carrelli = {}  # dizionario usato per gestire l'inserimento di valori nel carrello
+    tr = []  # struttura dati da restituire
     for record in ds:
-        #print('*')
-        idCliente=record[0]
-        tipoOp=record[1]
+        # print('*')
+        idCliente = record[0]
+        tipoOp = record[1]
         if idCliente not in carrelli:
-            carrelli[idCliente]={}
-        if tipoOp=='ADD':
+            carrelli[idCliente] = {}
+        if tipoOp == 'ADD':
             # (id_cliente1, 'ADD', id_prodottoA, qtA),
-            idProd=record[2]
-            qt=record[3]
+            idProd = record[2]
+            qt = record[3]
             if idProd not in carrelli[idCliente]:
-                carrelli[idCliente][idProd]=0
-            carrelli[idCliente][idProd]+=qt
-        elif tipoOp=='DROP':
-            idProd=record[2]
-            qt=record[3]
-            carrelli[idCliente][idProd]-=qt # non serve fare i controlli
-        elif tipoOp=='CHECKOUT':
-            #carico i valori in ds e tolgo i dati del cliente da carrelli
-            dic={}
+                carrelli[idCliente][idProd] = 0
+            carrelli[idCliente][idProd] += qt
+        elif tipoOp == 'DROP':
+            idProd = record[2]
+            qt = record[3]
+            carrelli[idCliente][idProd] -= qt  # non serve fare i controlli
+        elif tipoOp == 'CHECKOUT':
+            # carico i valori in ds e tolgo i dati del cliente da carrelli
+            dic = {}
             for idProd in carrelli[idCliente]:
                 qt = carrelli[idCliente][idProd]
-                if qt!=0:
-                    dic[idProd]=qt
-            tr.append( (idCliente, dic) )
-            carrelli[idCliente]={} # svuoto il carrello del cliente
+                if qt != 0:
+                    dic[idProd] = qt
+            tr.append((idCliente, dic))
+            carrelli[idCliente] = {}  #  svuoto il carrello del cliente
     return tr
 
 
@@ -295,24 +296,23 @@ def calcolaComposizione(ds):
 #
 def combinazioneMaggiore(ds):
     # Implementa il codice della funzione qua sotto. Questa riga puo' essere cancellata.
-    di={}
+    di = {}
     for el in ds:
-        #(id_cliente1, {id_prodottoA:qtA, id_prodottoB:qtB, ...})
-        idCliente=el[0]
+        # (id_cliente1, {id_prodottoA:qtA, id_prodottoB:qtB, ...})
+        idCliente = el[0]
         for idProdotto in el[1]:
             qt = el[1][idProdotto]
             st = str(idCliente)+'*'+str(idProdotto)
             if st not in di:
-                di[st]=0
-            di[st]+=qt
-    maxQt=-1
-    maxSt=''
+                di[st] = 0
+            di[st] += qt
+    maxQt = -1
+    maxSt = ''
     for el in di:
-        if di[el]>maxQt:
-            maxQt=di[el]
-            maxSt=el
+        if di[el] > maxQt:
+            maxQt = di[el]
+            maxSt = el
     return (maxSt, maxQt)
-
 
 
 ##########################################################
@@ -323,12 +323,11 @@ def combinazioneMaggiore(ds):
 # che il codice non dia errori in esecuzione.
 ##########################################################
 
-
 print('Esercizio %s.' % (nomeEsercizio))
 print('Ciao nome: %s, cognome: %s.' % (nome, cognome))
 
 print('1) Eseguo la funzione caricaDatiCarrelli: ')
-fnv = 'cart.csv'
+fnv = 'Esame-uni/Documents/Esame uni/2020_04_30/cart.csv'
 ds1 = caricaDatiCarrelli(fnv)
 print(ds1)
 
